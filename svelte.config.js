25a2b20a7f4aaa20d853c6f2e1e395512ce9f4b5
your-spellbindersmmo <spellbindersmmo@gmail.com>
2025-11-21 13:41:40 -0500

    Switch back to adapter-auto for Netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,12 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// Use Vercel adapter with explicit Node 20 runtime
-		adapter: adapter({
-			runtime: 'nodejs20.x'
-		})
+		adapter: adapter()
 	}
 };
 
